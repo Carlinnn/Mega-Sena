23c027d20c0aa0a23d8fae6757952c597b7e9dfe
Carlos Tavares <88689009+Carlinnn@users.noreply.github.com>
2023-01-24 17:37:13 -0400

    Update main.go

--- main.go
+++ main.go
@@ -8,26 +8,20 @@ import (
 )
 
 func main() {
-	// Gerar os números da Mega-Sena
 	numbers := generateMegaSenaNumbers()
 
-	// Ordenar os números
 	sort.Ints(numbers)
 
-	// Imprimir os números
 	fmt.Println(numbers)
 }
 
 func generateMegaSenaNumbers() []int {
-	// Inicializar o gerador de números aleatórios
 	rand.Seed(time.Now().UnixNano())
 
 	var numbers []int
 	for len(numbers) < 6 {
-		// Gerar um número aleatório entre 1 e 60
 		n := rand.Intn(60) + 1
 
-		// Verificar se o número já existe no slice
 		exists := false
 		for _, num := range numbers {
 			if num == n {
@@ -36,7 +30,6 @@ func generateMegaSenaNumbers() []int {
 			}
 		}
 
-		// Adicionar o número ao slice se ele ainda não existir
 		if !exists {
 			numbers = append(numbers, n)
 		}
